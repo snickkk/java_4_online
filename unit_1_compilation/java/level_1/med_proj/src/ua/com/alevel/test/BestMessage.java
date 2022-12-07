57a4d7ab6e3012f2974ce25ecd735f6f28dc36f3
snickkk <snickanton@gmail.com>
2022-12-08 01:53:29 +0200

    add all

---
+++ unit_1_compilation/java/level_1/med_proj/src/ua/com/alevel/test/BestMessage.java
@@ -0,0 +1,8 @@
+package ua.com.alevel.test;
+
+public class BestMessage {
+
+    public void showMessage () {
+        System.out.println("Ukraine the Best");
+    }
+}
--- unit_1_compilation/java/level_1/package/ua/com/alevel/BestUnit.java
+++ unit_1_compilation/java/level_1/package/ua/com/alevel/BestUnit.java
@@ -2,5 +2,5 @@ package ua.com.alevel;
 class BestUnit {
     public static void main(String[] args) {
         System.out.println("Best Unit");
+    }
 }
-
